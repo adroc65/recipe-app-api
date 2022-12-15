44a88f8d7474ea6f70513b69e5120f456c423712
adroc65 <aomodeo@gmail.com>
2022-12-14 21:42:22 -0600

    core/admin.py: Reparando error de LINT, no permite lineas mas largas de 80 caracteres

--- app/core/admin.py
+++ app/core/admin.py
@@ -45,4 +45,5 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Recipe)  # Permite la administracion desde el Admin de Django.
+# Permite la administracion desde el Admin de Django.
+admin.site.register(models.Recipe)
