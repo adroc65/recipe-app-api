a1e87a5fa1ac0e3c8d59ff29277be7219d9bff07
adroc65 <aomodeo@gmail.com>
2022-12-14 18:41:03 -0600

    test_recipe_api.py: Creado, pruebas fallan porque no se ha creado el serializers.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -45,4 +45,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Recipe)
+admin.site.register(models.Recipe)  # Permite la administracion desde el Admin de Django.
